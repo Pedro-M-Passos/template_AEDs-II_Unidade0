a52b613a006e36a60fd3613a02bb18d94692a348
Pedro Henrique Medina dos Passos <pedromedinap360@gmail.com>
2026-08-21 10:01:50 -0300

    Codigo Base

--- src/Produto.java
+++ src/Produto.java
@@ -1,13 +1,13 @@
 import java.text.NumberFormat;
 
-public class Produto {
+public abstract class Produto {
 	
 	private static final double MARGEM_PADRAO = 0.2;
-	private String descricao;
-	private double precoCusto;
-	private double margemLucro;
+	protected String descricao;
+	protected double precoCusto;
+	protected double margemLucro;
 	
-	/**
+     /**
      * Inicializador privado. Os valores default, em caso de erro, são:
      * "Produto sem descrição", R$ 0.00, 0.0  
      * @param desc Descrição do produto (mínimo de 3 caracteres)
@@ -17,7 +17,7 @@ public class Produto {
 	private void init(String desc, double precoCusto, double margemLucro) {
 		
 		if ((desc.length() >= 3) && (precoCusto > 0.0) && (margemLucro > 0.0)) {
-			this.descricao = desc;
+			descricao = desc;
 			this.precoCusto = precoCusto;
 			this.margemLucro = margemLucro;
 		} else {
@@ -25,35 +25,33 @@ public class Produto {
 		}
 	}
 	
-	/**
+     /**
      * Construtor completo. Os valores default, em caso de erro, são:
      * "Produto sem descrição", R$ 0.00, 0.0  
      * @param desc Descrição do produto (mínimo de 3 caracteres)
      * @param precoCusto Preço do produto (mínimo 0.01)
      * @param margemLucro Margem de lucro (mínimo 0.01)
      */
-	public Produto(String desc, double precoCusto, double margemLucro) {
+	protected Produto(String desc, double precoCusto, double margemLucro) {
 		init(desc, precoCusto, margemLucro);
 	}
 	
-	/**
+     /**
      * Construtor sem margem de lucro - fica considerado o valor padrão de margem de lucro.
      * Os valores default, em caso de erro, são:
      * "Produto sem descrição", R$ 0.00 
      * @param desc Descrição do produto (mínimo de 3 caracteres)
      * @param precoCusto Preço do produto (mínimo 0.01)
      */
-	public Produto(String desc, double precoCusto) {
+	protected Produto(String desc, double precoCusto) {
 		init(desc, precoCusto, MARGEM_PADRAO);
 	}
 	
-	 /**
+     /**
      * Retorna o valor de venda do produto, considerando seu preço de custo e margem de lucro.
      * @return Valor de venda do produto (double, positivo)
      */
-	public double valorDeVenda() {
-		return (precoCusto * (1.0 + margemLucro));
-	}
+	public abstract double valorDeVenda();
 	
 	/**
      * Descrição, em string, do produto, contendo sua descrição e o valor de venda.
@@ -65,6 +63,33 @@ public class Produto {
     	
     	NumberFormat moeda = NumberFormat.getCurrencyInstance();
     	
-		return String.format("NOME: " + descricao + ": " + moeda.format(valorDeVenda()));
+    	return String.format("NOME: " + descricao + ": " + moeda.format(valorDeVenda()));
 	}
+    
+    /**
+     * Igualdade de produtos: caso possuam o mesmo nome/descrição. 
+     * @param obj Outro produto a ser comparado 
+     * @return booleano true/false conforme o parâmetro possua a descrição igual ou não a este produto.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+    
+    /**
+     * Cria um produto a partir de uma linha de dados em formato texto. A linha de dados deve estar de acordo com a formatação
+     * "tipo;descrição;preçoDeCusto;margemDeLucro;[dataDeValidade]"
+     * ou o funcionamento não será garantido. Os tipos são 1, para produto não perecível; e 2, para perecível.
+     * @param linha Linha com os dados do produto a ser criado.
+     * @return Um produto com os dados recebidos
+     */
+    static Produto criarDoTexto(String linha) {
+    	return null;
+    }
+    	
+    /**
+     * Gera uma linha de texto a partir dos dados do produto.
+     * @return Uma string no formato "tipo;descrição;preçoDeCusto;margemDeLucro;[dataDeValidade]"
+     */
+    public abstract String gerarDadosTexto();
 }
